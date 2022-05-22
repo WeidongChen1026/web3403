f6c2a776f4ed4220defd3a933b1c0e507812f7c6
Catherine1213 <105783940+Catherine1213@users.noreply.github.com>
2022-05-22 22:01:36 +0800

    Add files via upload
    avoid flash

--- app/static/game1.js
+++ app/static/game1.js
@@ -1,36 +1,18 @@
       function random(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
 
-      window.onload = function(){
+      $(function(){
         $("#num_times").text(10)
+        $("#model_val").text(10)
+
         $("#score_val").text(0)
 
         var temp_val = random(1,100);
         $("#rd_val").text(temp_val);
-
-        var table = document.createElement("table");
-        table.setAttribute("border","1");
-        table.setAttribute("width","15%");
-
-        var cap = table.createCaption();
-        cap.innerHTML= "Ranking List";
-
-        for(var i =1;i<6;i++){
-          var tr = document.createElement("tr");
-          var td_na = document.createElement("td")
-          td_na.innerHTML= "Name";
-          var td_nu = document.createElement("td")
-          td_nu.innerHTML= 0;
-          tr.appendChild(td_na);
-          tr.appendChild(td_nu);
-
-          table.appendChild(tr);
-        }
-        document.getElementById("tb_cr").appendChild(table);
-
-      };
+      })
 
       function change_model(num_vals){
         $("#num_times").text(num_vals);
+        $("#model_val").text(num_vals);
         var ee = document.getElementById("num_val");
         ee.value = "";
       }
@@ -52,24 +34,22 @@
             $("#tips").text("Your guess is a little low");
           }else{
             $("score_val").html($("#score_val").html()+1)
-            $("#tips").text("Congratulations!!!");
-            // pass();
-            document.getElementById("result").innerHTML="Guess number <br> The number I guessed is(加入猜了的数字)," +
-                " and there is still (还剩几次) chances remain";
-            document.getElementById("share").style.display = "inline";
-            document.getElementById('share').addEventListener('click', function() {
-              var isCopyed = copyDiv();
-              tips(isCopyed);
-		}, false);
-            window.location.reload();
-            reload_i();
-            window.location.reload();
+            alert("Congratulations!!!");
+            
+            re_init()
+
           }
         }
       }
 
-      function reload_i(){
-        window.location.reload();
+      function re_init(){
+        var model_val = $("#model_val").text()
+        $("#num_times").text(model_val)
+
+        var temp_val = random(1,100);
+        $("#rd_val").text(temp_val);
+
+        $("#num_val").val('')
       }
 
       function tips(status) {
